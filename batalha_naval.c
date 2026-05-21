4d54844d4ce61a6d6b28ef87b26ec7a5250c3145
KessyJH <202601369041@alunos.estacio.br>
2026-05-21 20:14:42 -0300

    Update batalha_naval.c

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <locale.h>
 
+
+// Definindo o tamanho do tabuleiro
+#define TAM 10
+
+// Função para posicionar um navio no tabuleiro
+void posicionarNavio(int tabuleiro[TAM][TAM], int tamanho, int linha, int coluna, char orientacao) {
+    for (int i = 0; i < tamanho; i++) {
+        if (orientacao == 'H') { // Horizontal
+            tabuleiro[linha][coluna + i] = 3;
+        } else if (orientacao == 'V') { // Vertical
+            tabuleiro[linha + i][coluna] = 3;
+        }
+    }
+}
+
 int main() {
     setlocale(LC_ALL, "Pt-BR.UTF-8");
 
-    int tabluleiro[10][10] = {0}; // Tabuleiro de 10x10, inicializado com zeros
-    int navio[3][1] = {{1}, {1}, {1}};
-    char letra[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    char *numero[10] = {"1","2","3","4","5","6","7","8","9","10"};
+    int tabuleiro[TAM][TAM] = {0}; // Tabuleiro inicializado com zeros
+    char letra[TAM] = {'A','B','C','D','E','F','G','H','I','J'};
+    char *numero[TAM] = {"1","2","3","4","5","6","7","8","9","10"};
 
+    // Posicionar navio de tamanho 3 na linha 6, coluna 7, vertical
+    posicionarNavio(tabuleiro, 3, 3, 6, 'V');
 
-    //Exibindo o tabuleiro
-
+    // Exibindo o tabuleiro
     printf("Tabuleiro de Batalha Naval:\n");
-    printf("  ");
+    printf("   ");
 
-    //Posiciona o navio no tabuleiro.
-    for (int i = 0; i < 3; i++) {
-        tabluleiro[i][0] = navio[i][0]; // Posiciona o navio na primeira coluna do tabuleiro
-        tabluleiro[i][2] = navio[i][1]; // Posiciona o navio na segunda coluna do tabuleiro
-        tabluleiro[i][4] = navio[i][1]; // Posiciona o navio na terceira coluna do tabuleiro
-    }
-    for (int k = 0; k < 10; k++) {
-        printf(" %2c", letra[k]);// Exibe as letras de A a J no topo do tabuleiro
+    // Imprime as letras das colunas
+    for (int k = 0; k < TAM; k++) {
+        printf(" %2c", letra[k]);
     }
     printf("\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%2s|", numero[i]);
-        for (int j = 0; j < 10; j++) {
-            printf(" %d ", tabluleiro[i][j]);
+
+    // Imprime o tabuleiro com os números das linhas
+    for (int i = 0; i < TAM; i++) {
+        printf(" %2s|", numero[i]);
+        for (int j = 0; j < TAM; j++) {
+            printf(" %d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
